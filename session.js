73f75ae81df2d9bf917a8d126c9f989b160a2c21
arutobot <95910521+arutobot@users.noreply.github.com>
2021-12-11 11:13:37 +0800

    Create session.js

---
+++ session.js
@@ -0,0 +1,8 @@
+
+{
+        "clientID": "y/4QN9PgORan13nLGG1L4g==",
+        "serverToken": "1@t9Y0jhMX941nIN61IviuuhYOMhcWDw4xliUSLWkib4YiYXVV31yh7HQtyJ1Mw3lY+AEzXozkjMNwAw=
+        "clientToken": "2f9+6N5DECG41BTyW6Y2qeLXy5efbdFzA0qgJ0OrEvI=",
+        "encKey": "tv1exvIcQlceIRwrQCfGXnmGzo7IPIMYSUcsuCF1CAg=",
+        "macKey": "7uOYKSw7FMPxDgJ1gsmTajVk3J/pGY3JAkF5odaL48E="
+}

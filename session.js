ccf9530aaeed35ca410e3176a9f5aabb89bdd2e7
arutobot <95910521+arutobot@users.noreply.github.com>
2021-12-11 12:23:09 +0800

    Update session.js

--- session.js
+++ session.js
@@ -1,7 +1,7 @@
 
 {
         "clientID": "y/4QN9PgORan13nLGG1L4g==",
-        "serverToken": "1@t9Y0jhMX941nIN61IviuuhYOMhcWDw4xliUSLWkib4YiYXVV31yh7HQtyJ1Mw3lY+AEzXozkjMNwAw=
+        "serverToken": "1@t9Y0jhMX941nIN61IviuuhYOMhcWDw4xliUSLWkib4YiYXVV31yh7HQtyJ1Mw3lY+AEzXozkjMNwAw==
         "clientToken": "2f9+6N5DECG41BTyW6Y2qeLXy5efbdFzA0qgJ0OrEvI=",
         "encKey": "tv1exvIcQlceIRwrQCfGXnmGzo7IPIMYSUcsuCF1CAg=",
         "macKey": "7uOYKSw7FMPxDgJ1gsmTajVk3J/pGY3JAkF5odaL48E="
